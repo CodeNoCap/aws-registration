af21389019e9ba6f5f585988da965e6449706ed8
uykyleyuan <work.uykyleyuan@gmail.com>
2024-09-12 09:39:31 +0800

    Site reload timeout fix

--- script.js
+++ script.js
@@ -226,18 +226,4 @@ function isMobile() {
     return window.innerWidth <= 480;
 }
 
-const url = `https://aws-registration.vercel.app`; // Replace with your Render URL
-const interval = 30000; // Interval in milliseconds (30 seconds)
 
-function reloadWebsite() {
- axios.get(url)
-   .then(response => {
-     console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-   })
-   .catch(error => {
-     console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-   });
-}
-
-
-setInterval(reloadWebsite, interval);
--- server.js
+++ server.js
@@ -75,3 +75,19 @@ app.get('/api/get-last-refid', async (req, res) => {
 app.listen(10000, () => {
     console.log('Server running on port 10000');
 });
+
+const url = `https://aws-registration.vercel.app`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+function reloadWebsite() {
+ axios.get(url)
+   .then(response => {
+     console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+   })
+   .catch(error => {
+     console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+   });
+}
+
+
+setInterval(reloadWebsite, interval);
